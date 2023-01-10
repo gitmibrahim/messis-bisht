0b54f1829c967a9fde5231db01e5d7b4f1d3f415
ibrahim333999 <muhammed.ibrahim@logexa.com>
2023-01-10 09:05:45 +0200

    responding exactly on network response

--- ext/product-extractor.js
+++ ext/product-extractor.js
@@ -1,4 +1,4 @@
-(() => {
+chrome.runtime.sendMessage({ listenOnRequest: 'https://www.amazon.com/associates/sitestripe/getShortUrl?longUrl=' }, async () => {
   const descriptionLis = document.querySelectorAll('.a-list-item.a-size-base.a-color-secondary')
   const description = Array.from(descriptionLis).map(li => li.textContent)
   
@@ -21,73 +21,71 @@
   
   const deliveryAmount = document.querySelector('[data-csa-c-delivery-price]')
   
-  document.querySelector('[data-amzn-ss-show-text-popover] a').addEventListener('click', () => {
-    setTimeout(async () => {
-      const affLink = window.getSelection().toString()
+  const affLink = document.querySelector('#amzn-ss-text-shortlink-textarea').textContent
+
+  toDataURL = async (url) => await fetch(url).then(res => res.blob())
   
-      toDataURL = async (url) => await fetch(url).then(res => res.blob())
-      
-      download = async () => {
-        const a = document.createElement('a');
-        a.href = await toDataURL(document.querySelector('#landingImage').src);
-        a.download = document.querySelector('#landingImage').src.split('/').pop();
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
+  download = async () => {
+    const a = document.createElement('a');
+    a.href = await toDataURL(document.querySelector('#landingImage').src);
+    a.download = document.querySelector('#landingImage').src.split('/').pop();
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+  await download()
+
+  const product = {
+    info: {
+      slug: location.pathname.split('/')[1],
+      source: location.host.split('.')[1],
+      age: productAge,
+      colors: productColors,
+      sizes: productSizes,
+      affLink,
+    },
+    "@context": "https://schema.org/",
+    "@type": "Product",
+    name: productName,
+    description,
+    image: [
+      '../bishts/' + document.querySelector('li.image.item.itemNo0.maintain-height.selected img').src.split('/').pop()
+    ],
+    offers: {
+      "@type": "AggregateOffer",
+      price: document.querySelector('.a-price span.a-offscreen').textContent.substring(1),
+      priceCurrency: "USD",
+    },
+    brand: {
+      "@type": "Brand",
+      name: document.querySelector('#bylineInfo').textContent.substring(7)
+    },
+    aggregateRating: {
+      "@type": "AggregateRating",
+      ratingValue: document.querySelector('#acrPopover .a-icon-alt').textContent.slice(0, -15),
+      reviewCount: document.querySelector('#acrCustomerReviewText').textContent.match(/\d/g).join('')
+    },
+    shippingDetails: {
+      "@type": "OfferShippingDetails",
+      shippingRate: {
+        "@type": "MonetaryAmount",
+        value: deliveryAmount ? deliveryAmount.getAttribute('data-csa-c-delivery-price').substring(1) : null,
+        currency: "USD"
+      },
+      shippingDestination: {
+        "@type": "DefinedRegion",
+        addressCountry: "US"
       }
-      await download()
+    }
+  }
   
-      const product = {
-        slug: location.pathname.split('/')[1],
-        source: location.host.split('.')[1],
-        age: productAge,
-        colors: productColors,
-        sizes: productSizes,
-        description,
-        affLink,
-        "@context": "https://schema.org/",
-        "@type": "Product",
-        name: productName,
-        image: [
-          '../bishts/' + document.querySelector('li.image.item.itemNo0.maintain-height.selected img').src.split('/').pop()
-        ],
-        offers: {
-          "@type": "AggregateOffer",
-          price: document.querySelector('.a-price span.a-offscreen').textContent.substring(1),
-          priceCurrency: "USD",
-        },
-        brand: {
-          "@type": "Brand",
-          name: document.querySelector('#bylineInfo').textContent.substring(7)
-        },
-        aggregateRating: {
-          "@type": "AggregateRating",
-          ratingValue: document.querySelector('#acrPopover .a-icon-alt').textContent.slice(0, -15),
-          reviewCount: document.querySelector('#acrCustomerReviewText').textContent.match(/\d/g).join('')
-        },
-        shippingDetails: {
-          "@type": "OfferShippingDetails",
-          shippingRate: {
-            "@type": "MonetaryAmount",
-            value: deliveryAmount ? deliveryAmount.getAttribute('data-csa-c-delivery-price').substring(1) : null,
-            currency: "USD"
-          },
-          shippingDestination: {
-            "@type": "DefinedRegion",
-            addressCountry: "US"
-          }
-        }
-      }
-  
-      // fetch('/product', {
-      //   method: 'POST',
-      //   headers: {'Content-Type': 'application/json'},
-      //   body: JSON.stringify(product)
-      // })
-      console.log(product)
-    }, 1000)
-  })
-  
-  document.querySelector('[data-amzn-ss-show-text-popover] a').click()
-})()  
+  // fetch('/product', {
+  //   method: 'POST',
+  //   headers: {'Content-Type': 'application/json'},
+  //   body: JSON.stringify(product)
+  // })
+  console.log(product)
+});
+
+(() => {document.querySelector('[data-amzn-ss-show-text-popover] a').click()})()
 
--- ext/sw.js
+++ ext/sw.js
@@ -1,3 +1,14 @@
+chrome.runtime.onMessage.addListener( function (request, sender, sendResponse) {
+  chrome.webRequest.onCompleted.addListener((httpRequest) => {
+      if (httpRequest.url.startsWith(request.listenOnRequest)) {
+        sendResponse({ completedShortenURL: true })
+      }
+    },
+    {urls: ["<all_urls>"]}
+  )
+  return true
+})
+
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: {tabId: tab.id},
